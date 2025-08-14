4a0ff3e2d13a61d8ba7b5dd7b74dbce3ce866cbc
Ian Bouchard <ian@atvh.dev>
2025-08-14 07:45:30 -0400

    Make extensions and settings immutable

--- home/macos/cursor.nix
+++ home/macos/cursor.nix
@@ -3,10 +3,18 @@
   programs.vscode = {
     enable = true;
     package = pkgs.code-cursor;
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      # Vue extensions
-      vue.volar
+    mutableExtensionsDir = false;
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # Vue extensions
+        vue.volar
 
-    ] ++ (if local.vimMode then [ vscodevim.vim ] else []);
+      ] ++ (if local.vimMode then [ vscodevim.vim ] else []);
+
+      userSettings = {
+        "extensions.autoUpdate" = false;
+        "extensions.autoCheckUpdates" = false;
+      };
+    };
   };
 }
