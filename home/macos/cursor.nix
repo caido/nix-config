56e13666091b1b448a76f2d356f9e9646e5585cc
corb3nik <ian@atvh.dev>
2025-11-05 20:07:56 -0500

    Add rust analyzer cursor extension

--- home/macos/cursor.nix
+++ home/macos/cursor.nix
@@ -14,6 +14,7 @@
         aaron-bond.better-comments
         eamodio.gitlens
         github.vscode-github-actions
+        rust-lang.rust-analyzer
       ] ++ (if local.vimMode then [ vscodevim.vim ] else []);
 
       userSettings = {
